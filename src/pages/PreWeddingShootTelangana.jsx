fc9ee6cbd3eca38ba7f8239251f14fdfc0a6523d
sugurucreations-web <sugurucreations@gmail.com>
2026-01-24 08:51:00 +0000

    Create PreWeddingShootTelangana.jsx

---
+++ src/pages/PreWeddingShootTelangana.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+
+const PreWeddingShootTelangana = () => {
+  useEffect(() => {
+    document.title = "Pre Wedding Shoot in Telangana | Suguru Weddings";
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Pre Wedding Shoot in Telangana</h1>
+      <p>Beautiful pre-wedding photography across Telangana.</p>
+    </div>
+  );
+};
+
+export default PreWeddingShootTelangana;
