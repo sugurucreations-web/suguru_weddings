41c1f0b886ba1598fe69d9cd8c326399e70b39d1
sugurucreations-web <sugurucreations@gmail.com>
2026-01-24 08:50:40 +0000

    Create WeddingPhotographyHyderabad.jsx

---
+++ src/pages/WeddingPhotographyHyderabad.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+
+const WeddingPhotographyHyderabad = () => {
+  useEffect(() => {
+    document.title = "Wedding Photographer in Hyderabad | Suguru Weddings";
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Wedding Photographer in Hyderabad</h1>
+      <p>Premium wedding photography services in Hyderabad.</p>
+    </div>
+  );
+};
+
+export default WeddingPhotographyHyderabad;
