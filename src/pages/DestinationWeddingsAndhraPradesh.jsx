c4246b9d63309c8f4210692b4db8d3819afa614a
sugurucreations-web <sugurucreations@gmail.com>
2026-01-24 08:51:19 +0000

    Create DestinationWeddingsAndhraPradesh.jsx

---
+++ src/pages/DestinationWeddingsAndhraPradesh.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+
+const DestinationWeddingsAndhraPradesh = () => {
+  useEffect(() => {
+    document.title =
+      "Destination Weddings in Andhra Pradesh | Suguru Weddings";
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Destination Weddings in Andhra Pradesh</h1>
+      <p>Luxury destination wedding photography & videography.</p>
+    </div>
+  );
+};
+
+export default DestinationWeddingsAndhraPradesh;
