2eedc8cd9f174ed1cf91b460bbe559a251f0edfc
sugurucreations-web <sugurucreations@gmail.com>
2026-02-01 07:15:09 +0000

    Create Invoice.jsx

---
+++ src/pages/Invoice.jsx
@@ -0,0 +1,9 @@
+export default function Invoice() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        Invoice Portal
+      </h1>
+    </div>
+  );
+}
